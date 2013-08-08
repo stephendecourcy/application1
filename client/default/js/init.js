dca22217d002375ba4d777c3748a35319a86c9c7
stephen.decourcy@feedhenry.com <stephen.decourcy@feedhenry.com>
2013-08-08 11:37:45 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -22,8 +22,8 @@ function bindControl() {
     {
       act:'getConfig',
       "req": {
-         "key": "someValue", // send this value to the cloud
-         "peter": "16" // send this value to the cloud
+         "key": "someValue" // send this value to the cloud
+  //       "peter": "16" // send this value to the cloud
        }
     },
     function(res) {
